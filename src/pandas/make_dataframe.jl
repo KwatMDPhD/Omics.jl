6e569941eebc67696962059ae9c7aa505976ccc2
KwatME <kwat.me@icloud.com>
2021-10-06 02:08:25 -0600

    Email and move code/*

--- src/pandas/make_dataframe.jl
+++ src/pandas/make_dataframe.jl
@@ -1,18 +1,28 @@
 using DataFrames: DataFrame
 using PyCall: @py_str, PyObject
+using Pandas: reset_index, DataFrame as Pandas_DataFrame
+
 function make_dataframe(da::DataFrame)::PyObject
 
     py"""
     from pandas import DataFrame
     """
 
-    ie_ = 2:size(da)[2]-1
+    ie_ = 2:size(da)[2]
 
-    return py"DataFrame"(
-        data = Matrix(da[!, ie_]),
-        index = da[!, 1],
-        columns = names(da)[ie_],
-    )
+    na_ = names(da)
+
+    da = py"DataFrame"(data = Matrix(da[!, ie_]), index = da[!, 1], columns = na_[ie_])
+
+    da.index.name = na_[1]
+
+    return da
+
+end
+
+function make_dataframe(da::PyObject)::DataFrame
+
+    return DataFrame(reset_index(Pandas_DataFrame(da)))
 
 end
 
