6cce38550562c2fc663bba898759a4aa90e7db27
KwatMDPhD <kwat.me@icloud.com>
2021-12-06 06:36:05 -0700

    Housekeep 1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,20 @@
+TE = joinpath(tempdir(), "Kwat.test", "")
+
+if isdir(TE)
+
+    rm(TE; recursive = true)
+
+end
+
+mkdir(TE)
+
+println("Made ", TE)
+
+using Revise
+using BenchmarkTools
+
 using Kwat
+
+rm(TE; recursive = true)
+
+println("Removed ", TE)
