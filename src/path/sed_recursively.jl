698827c2277aae25b21f30d38af1fd3af02a1578
KwatMDPhD <kwat.me@icloud.com>
2022-02-19 11:48:32 -0700

    -t

--- src/path/sed_recursively.jl
+++ src/path/sed_recursively.jl
@@ -2,7 +2,7 @@ function sed_recursively(pa, pa_)
 
     for (be, af) in pa_
 
-        run(pipeline(`find $pa -type f -print0`, `xargs -0 -t sed -i "" "s/$be/$af/g"`))
+        run(pipeline(`find $pa -type f -print0`, `xargs -0 sed -i "" "s/$be/$af/g"`))
 
     end
 
