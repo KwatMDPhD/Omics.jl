33eb4f14809bdd97b2358211ce001a2d0490e396
KwatMDPhD <kwat.me@icloud.com>
2022-08-17 13:54:43 -0600

    Switched from @__DIR__ to pwd()

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,6 @@ sr = joinpath(dirname(@__DIR__), "src")
 
 sr_ = [na for na in readdir(sr)]
 
-ic_ = [
-    dirname(split(li, "\"")[2]) for
-    li in readlines(joinpath(sr, "OnePiece.jl")) if occursin("include", li)
-]
-
-symdiff(sr_, ic_)
-
 te_ = [splitext(na)[1] for na in readdir() if endswith(na, ".ipynb")]
 
 symdiff(sr_, te_)
