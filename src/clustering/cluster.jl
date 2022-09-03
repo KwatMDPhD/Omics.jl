2b6efaecfb65006e249cb0f34ce774cbb9c62e3b
KwatMDPhD <kwat.me@icloud.com>
2022-09-02 22:15:58 -0600

    Clustered

--- src/clustering/cluster.jl
+++ src/clustering/cluster.jl
@@ -1,6 +1,6 @@
 function cluster(sa_x_fe_x_nu, n_cl = 1; di = Euclidean(), li = :ward)
 
-    sa_x_sa_x_di = pairwise(di, sa_x_fe_x_nu, sa_x_fe_x_nu, dims = 1)
+    sa_x_sa_x_di = pairwise(di, sa_x_fe_x_nu, dims = 1)
 
     cl = hclust(sa_x_sa_x_di, linkage = li)
 
