455a5b3a5220f075dcfefb91de060a7ebff63306
KwatMDPhD <kwat.me@icloud.com>
2022-09-14 11:07:46 -0700

    Compared transcriptomics clusters

--- src/vector/sort_like.jl
+++ src/vector/sort_like.jl
@@ -1,7 +1,13 @@
-function sort_like(ve_...; re = false)
+function sort_like(ve_; re = false)
 
     id_ = sortperm(ve_[1], rev = re)
 
     [ve[id_] for ve in ve_]
+
+end
+
+function sort_like(ve_...; ke_ar...)
+
+    sort_like(ve_; ke_ar...)
 
 end
