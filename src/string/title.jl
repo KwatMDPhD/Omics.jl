37df9a01ee88f0251361bd52a6f408c5c6a328e3
KwatMDPhD <kwat.me@icloud.com>
2022-09-22 18:09:26 -0600

    GSEA bug

--- src/string/title.jl
+++ src/string/title.jl
@@ -1,5 +1,64 @@
 function title(st)
 
-    titlecase(Base.replace(st, "_" => " "))
+    st = replace(st, "_" => " ")
+
+    up_ = [isuppercase(ch) for ch in st]
+
+    st = titlecase(st)
+
+    st2 = ""
+
+    for (up, ch) in zip(up_, st)
+
+        if up
+
+            ch = uppercase(ch)
+
+        end
+
+        st2 *= ch
+
+    end
+
+    st = st2
+
+    for lo in [
+        "a",
+        "an",
+        "the",
+        "and",
+        "but",
+        "or",
+        "nor",
+        "at",
+        "by",
+        "for",
+        "from",
+        "in",
+        "into",
+        "of",
+        "off",
+        "on",
+        "onto",
+        "out",
+        "over",
+        "to",
+        "up",
+        "with",
+        "as",
+        "vs",
+    ]
+
+        st = replace(st, " $(titlecase(lo)) " => " $lo ")
+
+    end
+
+    for sh in ["m", "re", "s", "ve", "d"]
+
+        st = replace(st, "'$(titlecase(sh)) " => "'$sh ")
+
+    end
+
+    st
 
 end
