cabf646e462399613d6e7a8c4f39dbc4564e653c
KwatMDPhD <kwat.me@icloud.com>
2022-12-19 22:47:11 -0700

    Started to include.

---
+++ src/Path/rank.jl
@@ -0,0 +1,24 @@
+# TODO: Make testample.
+function rank(di)
+
+    na_ = readdir(di)
+
+    for (id, na) in enumerate(sort!(na_; by = na -> parse(Float64, rsplit(na, '.'; limit = 3)[1])))
+
+        na2 = "$id.$(join(rsplit(na, '.'; limit=3)[2:end], '.'))"
+
+        if na != na2
+
+            sr = joinpath(di, na)
+
+            de = joinpath(di, na2)
+
+            println("$sr ==> $de")
+
+            mv(sr, de)
+
+        end
+
+    end
+
+end
