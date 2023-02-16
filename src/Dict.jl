73b90cad79c989f1b4ae30f6a061493c584f6068
KwatMDPhD <kwat.me@icloud.com>
2023-02-16 15:06:57 -0700

    DataFrame-ed

--- src/Dict.jl
+++ src/Dict.jl
@@ -36,4 +36,26 @@ function print(ke_va; so = true, n = length(ke_va))
 
 end
 
+function set_with_first!(ke_va, ke, va)
+
+    if haskey(ke_va, ke)
+
+        vac = ke_va[ke]
+
+        if vac != va
+
+            println("$ke ➡️ $vac (➡️ $va)")
+
+        end
+
+    else
+
+        ke_va[ke] = va
+
+    end
+
+    return nothing
+
+end
+
 end
