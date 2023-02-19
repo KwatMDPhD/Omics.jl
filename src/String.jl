9d1f70d202cb9f334fa4b89aefb19ef43050a41e
KwatMDPhD <kwat.me@icloud.com>
2023-02-19 11:57:32 -0700

    String-ed

--- src/String.jl
+++ src/String.jl
@@ -66,13 +66,25 @@ function count_noun(n, st)
 
 end
 
+function _replace(st, be, af)
+
+    if contains(st, be)
+
+        st = replace(st, be => af)
+
+    end
+
+    return st
+
+end
+
 function title(st)
 
-    st = strip(st)
+    st = string(strip(st))
 
     ti = ""
 
-    for (up, ch) in zip((isuppercase(ch) for ch in st), titlecase(replace(st, '_' => ' ')))
+    for (up, ch) in zip((isuppercase(ch) for ch in st), titlecase(_replace(st, '_', ' ')))
 
         if up
 
@@ -85,40 +97,38 @@ function title(st)
     end
 
     for lo in (
-        "a",
-        "an",
-        "the",
-        "and",
-        "but",
-        "or",
-        "nor",
-        "at",
-        "by",
-        "for",
-        "from",
-        "in",
-        "into",
-        "of",
-        "off",
-        "on",
-        "onto",
-        "out",
-        "over",
-        "to",
-        "up",
-        "with",
-        "as",
-        "vs",
+        "'m ",
+        "'re ",
+        "'s ",
+        "'ve ",
+        "'d ",
+        " a ",
+        " an ",
+        " the ",
+        " and ",
+        " but ",
+        " or ",
+        " nor ",
+        " at ",
+        " by ",
+        " for ",
+        " from ",
+        " in ",
+        " into ",
+        " of ",
+        " off ",
+        " on ",
+        " onto ",
+        " out ",
+        " over ",
+        " to ",
+        " up ",
+        " with ",
+        " as ",
+        " vs ",
     )
 
-        ti = replace(ti, " $(titlecase(lo)) " => " $lo ")
-
-    end
-
-    # TODO: Time ''.
-    for sh in ("m", "re", "s", "ve", "d")
-
-        ti = replace(ti, "'$(titlecase(sh)) " => "'$sh ")
+        ti = _replace(ti, titlecase(lo), lo)
 
     end
 
@@ -126,7 +136,6 @@ function title(st)
 
 end
 
-# TODO: Time without `limit`.
 function split_and_get(st, de, id)
 
     return split(st, de; limit = id + 1)[id]
@@ -147,10 +156,7 @@ function transplant(st1, st2, de, id_)
 
     sp2_ = split(st2, de)
 
-    BioLab.Array.error_size(sp1_, sp2_)
-
-    # TODO: Time with `ifelse`.
-    return join(((sp1, sp2)[id] for (sp1, sp2, id) in zip(sp1_, sp2_, id_)), de)
+    return join(ifelse.((id == 1 for id in id_), sp1_, sp2_), de)
 
 end
 
