ccfa3cb81cf1762883d2388f7d2d55f95143c2d7
KwatMDPhD <kwat.me@icloud.com>
2023-02-20 16:40:14 -0700

    Simulate goes to the end.

--- src/VectorNumber.jl
+++ src/VectorNumber.jl
@@ -6,72 +6,6 @@ using Random: seed!
 
 using ..BioLab
 
-# TODO: Multiple-dispatch.
-function simulate(n; ra = BioLab.RA, di = "Normal", ho = "", ev = true)
-
-    if di == "Normal"
-
-        di = Normal()
-
-    else
-
-        erorr()
-
-    end
-
-    seed!(ra)
-
-    ra_ = rand(di, n)
-
-    po_ = shift_minimum(ra_, 0.0)
-
-    sort!(po_)
-
-    ne_ = reverse(-po_)
-
-    if isempty(ho)
-
-        nem_ = ne_
-
-    elseif ho == "deep"
-
-        nem_ = ne_ * 2.0
-
-    elseif ho == "long"
-
-        nem_ = Vector{Float64}(undef, n * 2 - 1)
-
-        for (id, ne) in enumerate(ne_)
-
-            id2 = id * 2
-
-            nem_[id2 - 1] = ne
-
-            if id < n
-
-                nem_[id2] = (ne + ne_[id + 1]) / 2.0
-
-            end
-
-        end
-
-    else
-
-        error()
-
-    end
-
-    if !ev
-
-        nem_ = nem_[1:(end - 1)]
-
-    end
-
-    # TODO: Preallocate.
-    return vcat(nem_, po_)
-
-end
-
 function get_area(nu_)
 
     return sum(nu_) / length(nu_)
@@ -167,6 +101,72 @@ function skip_nan_and_apply!(nu_, fu)
     end
 
     return nothing
+
+end
+
+# TODO: Multiple-dispatch.
+function simulate(n; ra = BioLab.RA, di = "Normal", ho = "", ev = true)
+
+    if di == "Normal"
+
+        di = Normal()
+
+    else
+
+        erorr()
+
+    end
+
+    seed!(ra)
+
+    ra_ = rand(di, n)
+
+    po_ = shift_minimum(ra_, 0.0)
+
+    sort!(po_)
+
+    ne_ = reverse(-po_)
+
+    if isempty(ho)
+
+        nem_ = ne_
+
+    elseif ho == "deep"
+
+        nem_ = ne_ * 2.0
+
+    elseif ho == "long"
+
+        nem_ = Vector{Float64}(undef, n * 2 - 1)
+
+        for (id, ne) in enumerate(ne_)
+
+            id2 = id * 2
+
+            nem_[id2 - 1] = ne
+
+            if id < n
+
+                nem_[id2] = (ne + ne_[id + 1]) / 2.0
+
+            end
+
+        end
+
+    else
+
+        error()
+
+    end
+
+    if !ev
+
+        nem_ = nem_[1:(end - 1)]
+
+    end
+
+    # TODO: Preallocate.
+    return vcat(nem_, po_)
 
 end
 
