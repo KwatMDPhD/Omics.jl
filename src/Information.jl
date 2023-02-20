37b453bb68e82370a9461147d798c9629c6255a8
KwatMDPhD <kwat.me@icloud.com>
2023-02-20 16:25:21 -0700

    Information-ed

--- src/Information.jl
+++ src/Information.jl
@@ -48,24 +48,6 @@ function get_kullback_leibler_divergence(nu1_, nu2_)
 
 end
 
-function get_symmetric_kullback_leibler_divergence(nu1_, nu2_, nu_; we1 = 1 / 2, we2 = 1 / 2)
-
-    return [
-        nu1 * we1 + nu2 * we2 for (nu1, nu2) in
-        zip(get_kullback_leibler_divergence(nu1_, nu_), get_kullback_leibler_divergence(nu2_, nu_))
-    ]
-
-end
-
-function get_antisymmetric_kullback_leibler_divergence(nu1_, nu2_, nu_; we1 = 1 / 2, we2 = 1 / 2)
-
-    return [
-        nu1 * we1 - nu2 * we2 for (nu1, nu2) in
-        zip(get_kullback_leibler_divergence(nu1_, nu_), get_kullback_leibler_divergence(nu2_, nu_))
-    ]
-
-end
-
 function get_thermodynamic_breadth(nu1_, nu2_)
 
     return [
@@ -84,6 +66,24 @@ function get_thermodynamic_depth(nu1_, nu2_)
             get_kullback_leibler_divergence(nu1_, nu2_),
             get_kullback_leibler_divergence(nu2_, nu1_),
         )
+    ]
+
+end
+
+function get_symmetric_kullback_leibler_divergence(nu1_, nu2_, nu_; we1 = 0.5, we2 = 0.5)
+
+    return [
+        nu1 * we1 + nu2 * we2 for (nu1, nu2) in
+        zip(get_kullback_leibler_divergence(nu1_, nu_), get_kullback_leibler_divergence(nu2_, nu_))
+    ]
+
+end
+
+function get_antisymmetric_kullback_leibler_divergence(nu1_, nu2_, nu_; we1 = 0.5, we2 = 0.5)
+
+    return [
+        nu1 * we1 - nu2 * we2 for (nu1, nu2) in
+        zip(get_kullback_leibler_divergence(nu1_, nu_), get_kullback_leibler_divergence(nu2_, nu_))
     ]
 
 end
