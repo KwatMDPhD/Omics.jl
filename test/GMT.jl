513b97efb59c07cbe49bbb208376a7ddc901b20e
KwatMDPhD <kwat.me@icloud.com>
2023-06-09 16:51:18 -0600

    GMT

--- test/GMT.jl
+++ test/GMT.jl
@@ -2,24 +2,10 @@ include("environment.jl")
 
 # ---- #
 
-da = joinpath(pkgdir(BioLab), "data", "GMT")
+da = joinpath(DA, "GMT")
 
-gm1 = joinpath(da, "h.all.v7.1.symbols.gmt")
+for (na, re) in (("h.all.v7.1.symbols.gmt", 50), ("c2.all.v7.1.symbols.gmt", 5529))
 
-le1 = 50
-
-gm2 = joinpath(da, "c2.all.v7.1.symbols.gmt")
-
-le2 = 5529
-
-# ---- #
-
-for (gm, re) in ((gm1, le1), (gm2, le2))
-
-    @test length(BioLab.GMT.read(gm)) == re
+    @test length(BioLab.GMT.read(joinpath(da, na))) == re
 
 end
-
-# ---- #
-
-@test length(merge((BioLab.GMT.read(gm) for gm in (gm1, gm2))...)) == le1 + le2
