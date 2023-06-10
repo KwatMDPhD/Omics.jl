62f50a4607f9aacad787ee69fe108dbe85b41434
KwatMDPhD <kwat.me@icloud.com>
2023-06-10 14:07:53 -0600

    Number.jl.

--- test/String.jl
+++ test/String.jl
@@ -2,6 +2,23 @@ include("environment.jl")
 
 # ---- #
 
+for (nu, re) in (
+    (-0, "0"),
+    (0, "0"),
+    (-0.0, "0"),
+    (0.0, "0"),
+    (1 / 3, "0.3333"),
+    (0.1234567890123456789, "0.1235"),
+    (0.001, "0.001"),
+    (0.000001, "1e-06"),
+)
+
+    @test isequal(BioLab.Number.format(nu), re)
+
+end
+
+# ---- #
+
 for (n, re) in ((1, "C..."), (2, "Ca..."), (3, "Can"), (4, "Can"))
 
     @test BioLab.String.limit("Can", n) == re
