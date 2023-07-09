084f8145d3c321ca57a92d73ee21d56e82a1b4b0
KwatMDPhD <kwat.me@icloud.com>
2023-07-08 23:02:20 -0600

    Collection

--- src/Collection.jl
+++ src/Collection.jl
@@ -1,96 +1,22 @@
 module Collection
 
-using StatsBase: countmap
+function is_in(an_id, an1_)
 
-function _countmap_string(an_)
-
-    join(("$n $an" for (an, n) in countmap(an_) if 1 < n), ".\n")
-
-end
-
-function error_duplicate(an_)
-
-    if isempty(an_)
-
-        error("Collection is empty.")
-
-    end
-
-    if !allunique(an_)
-
-        st = _countmap_string(an_)
-
-        error("Collection has duplicates.\n$st.")
-
-    end
-
-end
-
-function error_no_change(an_)
-
-    if allequal(an_)
-
-        st = _countmap_string(an_)
-
-        error("Collection has no change. $st.")
-
-    end
-
-end
-
-function index(an_)
-
-    ty = eltype(an_)
-
-    an_id = Dict{ty, Int}()
-
-    id_an = Dict{Int, ty}()
-
-    for (id, an) in enumerate(an_)
-
-        an_id[an] = id
-
-        id_an[id] = an
-
-    end
-
-    an_id, id_an
-
-end
-
-function is_in(an_, an1_)
-
-    n = length(an_)
-
-    bo_ = Vector{Bool}(undef, n)
-
-    for id in 1:n
-
-        bo_[id] = an_[id] in an1_
-
-    end
-
-    bo_
-
-end
-
-function is_in(an_id::AbstractDict, an1_)
-
-    bo_ = fill(false, length(an_id))
+    bi_ = falses(length(an_id))
 
     for an1 in an1_
 
-        id = get(an_id, an1, 0)
+        id = get(an_id, an1, nothing)
 
-        if !iszero(id)
+        if !isnothing(id)
 
-            bo_[id] = true
+            bi_[id] = true
 
         end
 
     end
 
-    bo_
+    bi_
 
 end
 
--- test/Collection.jl
+++ test/Collection.jl
@@ -1,106 +1,12 @@
 using Test: @test
 
-# ---- #
-
-for an_ in (
-    (),
-    (1, 1),
-    [1, 1],
-    [1, 2, 2],
-    [1 1; 2 2],
-    [1 2; 1 2],
-    [1 2; 3 1],
-    [nothing, nothing],
-    [missing, missing],
-    [NaN, NaN],
-    [Inf, Inf],
-    [0.5, 1 / 2, 1 // 2],
-    ['a', 'b', 'b'],
-)
-
-    @test @is_error BioLab.Collection.error_duplicate(an_)
-
-end
+using BioLab
 
 # ---- #
 
-for an_ in (
-    (1, 2),
-    [],
-    [1, 2],
-    [1, 2, 3],
-    [1 2; 3 4],
-    [nothing, missing, NaN, -Inf, Inf, -0.0, 0.0],
-    [1.0, 2],
-    ['a', 'b', 'c'],
-    ['a', 'b', 'c', "c"],
-)
-
-    @test !@is_error BioLab.Collection.error_duplicate(an_)
-
-end
-
-# ---- #
-
-for an_ in (
-    (),
-    (1, 1),
-    [1, 1],
-    [1, 1, 1],
-    [1 1],
-    [1 1; 1 1],
-    [nothing, nothing],
-    [missing, missing],
-    [NaN, NaN],
-    [-Inf, -Inf],
-    [Inf, Inf],
-    ['a', 'a'],
-    ["a", "a"],
-)
-
-    @test @is_error BioLab.Collection.error_no_change(an_)
-
-end
-
-# ---- #
-
-for an_ in (
-    (1, 2),
-    [],
-    [1, 2],
-    [1, 2, 3],
-    [1 2],
-    [1 1; 1 2],
-    [1.0, 2],
-    [nothing, missing],
-    [nothing, NaN],
-    [missing, NaN],
-    [-Inf, Inf],
-    [-0.0, 0.0],
-    ['a', 'b'],
-    ['a', "a"],
-)
-
-    @test !@is_error BioLab.Collection.error_no_change(an_)
-
-end
-
-# ---- #
-
-@test BioLab.Collection.index(("Aa", "Ii", "Uu", "Ee", "Oo")) == (
-    Dict("Aa" => 1, "Ii" => 2, "Uu" => 3, "Ee" => 4, "Oo" => 5),
-    Dict(1 => "Aa", 2 => "Ii", 3 => "Uu", 4 => "Ee", 5 => "Oo"),
-)
-
-# ---- #
-
-# TODO: Try with Tuple.
 an_ = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K']
 
 an1_ = ['1', '2', 'K']
-
-@test BioLab.Collection.is_in(an_, an1_) ==
-      [false, true, false, false, false, false, false, false, false, false, false, false, true]
 
 @test BioLab.Collection.is_in(Dict('A' => 1, '2' => 2, '3' => 3, 'Q' => 4, 'K' => 5), an1_) ==
       [false, true, false, false, true]
@@ -108,22 +14,35 @@ an1_ = ['1', '2', 'K']
 @test BioLab.Collection.is_in(Dict('A' => 5, '2' => 4, '3' => 3, 'Q' => 2, 'K' => 1), an1_) ==
       [true, false, false, true, false]
 
-@test BioLab.Collection.is_in(an_, an1_) ==
-      BioLab.Collection.is_in(an_, Set(an1_)) ==
-      BioLab.Collection.is_in(Dict(ca => id for (id, ca) in enumerate(an_)), an1_)
-
 # ---- #
 
-fe_ = reverse!(BioLab.Table.read(joinpath(DA, "gene_x_statistic_x_number.tsv"))[!, 1])
+di = joinpath(BioLab.DA, "FeatureSetEnrichment")
 
-fe1_ = BioLab.GMT.read(joinpath(DA, "c2.all.v7.1.symbols.gmt"))["COLLER_MYC_TARGETS_UP"]
+fe_ = reverse!(BioLab.Table.read(joinpath(di, "gene_x_statistic_x_number.tsv"))[!, 1])
 
-# 454.167 μs (2 allocations: 19.67 KiB)
-#@btime BioLab.Collection.is_in($fe_, $(Set(fe1_)));
+fe1_ = BioLab.GMT.read(joinpath(di, "c2.all.v7.1.symbols.gmt"))["COLLER_MYC_TARGETS_UP"]
 
-# 740.875 μs (2 allocations: 19.67 KiB)
-#@btime BioLab.Collection.is_in($fe_, $fe1_);
+fe1s = Set(fe1_)
 
-# 616.616 ns (2 allocations: 19.67 KiB)
-# 929.688 ns (2 allocations: 19.67 KiB)
-#@btime BioLab.Collection.is_in($(Dict(fe => id for (id, fe) in enumerate(fe_))), $fe1_);
+fe_id = Dict(fe => id for (id, fe) in enumerate(fe_))
+
+# 444.657 ns (7 allocations: 1.13 KiB)
+#@btime Set($fe1_);
+
+# 510.250 μs (7 allocations: 800.92 KiB)
+#@btime Dict(fe => id for (id, fe) in enumerate($fe_));
+
+# 738.584 μs (2 allocations: 19.67 KiB)
+#@btime [fe in $fe1_ for fe in $fe_];
+
+# 469.875 μs (2 allocations: 19.67 KiB)
+#@btime [fe in $fe1s for fe in $fe_];
+
+# 740.458 μs (3 allocations: 6.84 KiB)
+#@btime in($fe1_).($fe_);
+
+# 476.167 μs (3 allocations: 6.84 KiB)
+#@btime in($fe1s).($fe_);
+
+# 384.698 ns (2 allocations: 2.66 KiB)
+#@btime BioLab.Collection.is_in($fe_id, $fe1_);
