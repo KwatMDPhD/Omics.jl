6e71e7678f0af0092fd2617154fbde5e6657c01a
KwatMDPhD <kwat.me@icloud.com>
2023-07-21 16:46:21 -0600

    Time

--- test/Time.jl
+++ test/Time.jl
@@ -6,4 +6,4 @@ using BioLab
 
 st = BioLab.Time.stamp()
 
-@test contains(st, r"^[\d]{4}_[\d]{1}_[\d]{2}_[\d]{2}_[\d]{2}_[\d]{2}_[\d]{3}$")
+@test contains(st, r"^[\d]{4}_[\d]{1,2}_[\d]{1,2}_[\d]{1,2}_[\d]{1,2}_[\d]{1,2}_[\d]{1,3}$")
