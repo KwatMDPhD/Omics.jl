84a5e171c97c254d0a91825a83ae47e168800503
KwatMDPhD <kwat.me@icloud.com>
2023-10-18 15:42:32 -0600

    GMTY.

--- test/GMT.jl
+++ test/GMT.jl
@@ -20,6 +20,6 @@ for (gm, re) in (("h.all.v7.1.symbols.gmt", 50), ("c2.all.v7.1.symbols.gmt", 552
 
     # 350.416 μs (7717 allocations: 974.86 KiB)
     # 26.849 ms (515903 allocations: 51.28 MiB)
-    @btime BioLab.GMT.read($gm)
+    #@btime BioLab.GMT.read($gm)
 
 end
