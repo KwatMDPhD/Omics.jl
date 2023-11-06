cea21c2f61497a023de52d46508c0f28fa1483f5
KwatMDPhD <kwat.me@icloud.com>
2023-11-06 14:53:29 -0700

    Refactored FeatureXSample.

--- src/FeatureXSample.jl
+++ src/FeatureXSample.jl
@@ -1,26 +1,32 @@
 module FeatureXSample
 
-using Downloads: download
-
 using StatsBase: median
 
 using ..Nucleus
 
-function summarize(na_, an___)
+function initialize_block()
 
-    for (na, an_) in zip(na_, an___)
-
-        println("🔦 $na\n$(Nucleus.Collection.count_sort_string(an_))")
-
-    end
+    String[], String[], Vector{String}[], Vector{String}[], Matrix[]
 
 end
 
-function match(co1_, co2_, ro_x_co1_x_an, ro_x_co2_x_an)
+function push_block!(ts_, nar_, ro___, co___, ma_, ts, nar, ro_, co_, ma)
 
-    co_ = intersect(co1_, co2_)
+    push!(ts_, ts)
 
-    n = lastindex(co_)
+    push!(nar_, nar)
+
+    push!(ro___, ro_)
+
+    push!(co___, co_)
+
+    push!(ma_, ma)
+
+    nothing
+
+end
+
+function _error_0(n)
 
     if iszero(n)
 
@@ -28,96 +34,134 @@ function match(co1_, co2_, ro_x_co1_x_an, ro_x_co2_x_an)
 
     end
 
-    @info "🪞 $(lastindex(co1_)) ∩ $(lastindex(co2_)) = $n." co_
+end
 
-    co_, ro_x_co1_x_an[:, indexin(co_, co1_)], ro_x_co2_x_an[:, indexin(co_, co2_)]
+function _log_intersection(an___, it_)
+
+    n = lastindex(it_)
+
+    @info "$(join((lastindex(an_) for an_ in an___), " ∩ ")) = $n." it_
+
+    _error_0(n)
+
+end
+
+function intersect_column!(co___, ma_)
+
+    it_ = intersect(co___...)
+
+    _log_intersection(co___, it_)
+
+    for (id, (co_, ma)) in enumerate(zip(co___, ma_))
+
+        ma_[id] = ma[:, indexin(it_, co_)]
+
+    end
+
+    it_
+
+end
+
+# TODO: Remove.
+function intersect_column(co1_, co2_, ro_x_co1_x_an, ro_x_co2_x_an)
+
+    ma_ = [ro_x_co1_x_an, ro_x_co2_x_an]
+
+    it_ = intersect_column!((co1_, co2_), ma_)
+
+    it_, ma_...
+
+end
+
+function write_block(ts_, nar_, ro___, co_, ma_)
+
+    for (ts, nar, ro_, ma) in zip(ts_, nar_, ro___, ma_)
+
+        Nucleus.DataFrame.write(ts, nar, ro_, co_, ma)
+
+        @info ts nar ro_ co_ ma
+
+    end
+
+end
+
+function count_unique(na_, an___)
+
+    for (na, an_) in zip(na_, an___)
+
+        @info "$na\n$(Nucleus.Collection.count_sort_string(an_))"
+
+    end
+
+end
+
+function _plot(ro_x_co_x_nu, ro_, co_, nar, nac, nan)
+
+    layout = Dict("title" => Dict("text" => nan))
+
+    Nucleus.Plot.plot_heat_map("", ro_x_co_x_nu; y = ro_, x = co_, nar, nac, layout)
+
+    Nucleus.Plot.plot_histogram("", (vec(ro_x_co_x_nu),); layout)
 
 end
 
 function transform(
-    di,
-    fe_,
-    sa_,
-    fe_x_sa_x_nu;
-    fe_fe2 = Dict{String, String}(),
+    ro_,
+    co_,
+    ro_x_co_x_nu;
+    ro_ro2 = Dict{String, String}(),
     fu = median,
     ty = Float64,
     lo = false,
-    naf = "Feature",
-    nas = "Sample",
+    nar = "Row",
+    nac = "Column",
     nan = "Number",
 )
 
-    Nucleus.Error.error_missing(di)
+    Nucleus.Error.error_bad(isnothing, ro_)
 
-    Nucleus.Error.error_bad(isnothing, fe_)
+    Nucleus.Error.error_bad(ismissing, ro_)
 
-    Nucleus.Error.error_bad(ismissing, fe_)
+    Nucleus.Error.error_bad(Nucleus.String.is_bad, ro_)
 
-    Nucleus.Error.error_bad(Nucleus.String.is_bad, fe_)
+    Nucleus.Error.error_duplicate(co_)
 
-    Nucleus.Error.error_duplicate(sa_)
+    Nucleus.Error.error_bad(isnothing, co_)
 
-    Nucleus.Error.error_bad(isnothing, sa_)
+    Nucleus.Error.error_bad(ismissing, co_)
 
-    Nucleus.Error.error_bad(ismissing, sa_)
+    Nucleus.Error.error_bad(Nucleus.String.is_bad, co_)
 
-    Nucleus.Error.error_bad(Nucleus.String.is_bad, sa_)
+    Nucleus.Error.error_bad(isnothing, ro_x_co_x_nu)
 
-    Nucleus.Error.error_bad(isnothing, fe_x_sa_x_nu)
+    Nucleus.Error.error_bad(ismissing, ro_x_co_x_nu)
 
-    Nucleus.Error.error_bad(ismissing, fe_x_sa_x_nu)
+    Nucleus.Error.error_bad(!isfinite, ro_x_co_x_nu)
 
-    Nucleus.Error.error_bad(!isfinite, fe_x_sa_x_nu)
+    _plot(ro_x_co_x_nu, ro_, co_, nar, nac, nan)
 
-    nafc = Nucleus.Path.clean(naf)
-
-    nasc = Nucleus.Path.clean(nas)
-
-    nanc = Nucleus.Path.clean(nan)
-
-    layout = Dict("title" => Dict("text" => nan))
-
-    Nucleus.Plot.plot_heat_map(
-        joinpath(di, "$(nafc)_x_$(nasc)_x_$(nanc).html"),
-        fe_x_sa_x_nu;
-        y = fe_,
-        x = sa_,
-        nar = naf,
-        nac = nas,
-        layout,
-    )
-
-    na = "$(nafc)$(nasc)$(nanc)"
-
-    Nucleus.Plot.plot_histogram(joinpath(di, "$na.html"), (vec(fe_x_sa_x_nu),); layout)
-
-    if !isempty(fe_fe2)
+    if !isempty(ro_ro2)
 
         # TODO: Generalize the "ENSG" logic.
-        Nucleus.Gene.rename!(fe_, fe_fe2)
+        Nucleus.Gene.rename!(ro_, ro_ro2)
 
     end
 
-    if !allunique(fe_)
+    if !allunique(ro_)
 
-        fe_, fe_x_sa_x_nu = Nucleus.Matrix.collapse(fu, ty, fe_, fe_x_sa_x_nu)
+        ro_, ro_x_co_x_nu = Nucleus.Matrix.collapse(fu, ty, ro_, ro_x_co_x_nu)
 
     end
 
     if lo
 
-        fe_x_sa_x_nu .= log2.(fe_x_sa_x_nu .+ 1)
-
-        Nucleus.Plot.plot_histogram(
-            joinpath(di, "$(na)_plus1_log2.html"),
-            (vec(fe_x_sa_x_nu),);
-            layout = Dict("title" => Dict("text" => "$nan (+1 Log2)")),
-        )
+        ro_x_co_x_nu .= log2.(ro_x_co_x_nu .+ 1)
 
     end
 
-    fe_, fe_x_sa_x_nu
+    _plot(ro_x_co_x_nu, ro_, co_, nar, nac, "$nan (Final)")
+
+    ro_, ro_x_co_x_nu
 
 end
 
--- test/FeatureXSample.jl
+++ test/FeatureXSample.jl
@@ -4,18 +4,80 @@ using Nucleus
 
 # ---- #
 
-for (na_, an___) in ((
-    ["Row", "Column 1", "Column 2", "Column 3"],
-    ([1, 2, 3], ['A', 'B', 'C'], ['A', 'B', 'B'], ['C', 'C', 'C']),
-),)
+const TS_, NAR_, RO___, CO___, MA_ = Nucleus.FeatureXSample.initialize_block()
 
-    Nucleus.FeatureXSample.summarize(na_, an___)
+# ---- #
+
+for (bl, re) in
+    zip((TS_, NAR_, RO___, CO___, MA_), (String, String, Vector{String}, Vector{String}, Matrix))
+
+    @test eltype(bl) === re
+
+    @test isempty(bl)
 
 end
 
 # ---- #
 
-@test Nucleus.Error.@is Nucleus.FeatureXSample.match(1:2, 3:5, [1 2], [3 4 5])
+for (id, (ro_, co_)) in enumerate((
+    (["F 1 1", "F 1 2"], ["S1", "S2", "S3", "S4"]),
+    (["F 2 1", "F 2 2"], ["S2", "S3", "S4", "S5"]),
+    (["F 3 1", "F 3 2"], ["S3", "S4", "S5", "S6"]),
+))
+
+    ts = joinpath(Nucleus.TE, "$id.tsv")
+
+    nar = "Feature Set $id"
+
+    ma = Nucleus.Simulation.make_matrix_1n(Int, lastindex(ro_), lastindex(co_))
+
+    Nucleus.FeatureXSample.push_block!(TS_, NAR_, RO___, CO___, MA_, ts, nar, ro_, co_, ma)
+
+end
+
+# ---- #
+
+for bl in (TS_, NAR_, RO___, CO___, MA_)
+
+    @test lastindex(bl) === 3
+
+end
+
+# ---- #
+
+for ze in (0, 0.0)
+
+    @test Nucleus.Error.@is Nucleus.FeatureXSample._error_0(0)
+
+end
+
+# ---- #
+
+for nu in (1, 2.0)
+
+    @test !Nucleus.Error.@is Nucleus.FeatureXSample._error_0(nu)
+
+end
+
+# ---- #
+
+const CO_ = Nucleus.FeatureXSample.intersect_column!(CO___, MA_)
+
+# ---- #
+
+@test lastindex(CO_) === 2
+
+# ---- #
+
+for ma in MA_
+
+    @test size(ma, 2) === 2
+
+end
+
+# ---- #
+
+@test Nucleus.Error.@is Nucleus.FeatureXSample.intersect_column(1:2, 3:5, [1 2], [3 4 5])
 
 # ---- #
 
@@ -29,12 +91,12 @@ for (co1_, co2_, ro_x_co1_x_an, ro_x_co2_x_an, re) in (
     (1:3, 2:4, [1 2 3], [2 3 4], (2:3, [2 3], [2 3])),
 )
 
-    @test Nucleus.FeatureXSample.match(co1_, co2_, ro_x_co1_x_an, ro_x_co2_x_an) == re
+    @test Nucleus.FeatureXSample.intersect_column(co1_, co2_, ro_x_co1_x_an, ro_x_co2_x_an) == re
 
-    # 277.354 ns (14 allocations: 1.38 KiB)
-    # 287.865 ns (14 allocations: 1.41 KiB)
-    # 317.308 ns (14 allocations: 1.41 KiB)
-    #@btime Nucleus.FeatureXSample.match($co1_, $co2_, $ro_x_co1_x_an, $ro_x_co2_x_an)
+    # 441.919 ns (16 allocations: 1.52 KiB)
+    # 458.122 ns (16 allocations: 1.55 KiB)
+    # 488.186 ns (16 allocations: 1.55 KiB)
+    #@btime Nucleus.FeatureXSample.intersect_column($co1_, $co2_, $ro_x_co1_x_an, $ro_x_co2_x_an)
 
 end
 
@@ -44,40 +106,45 @@ end
 
 # ---- #
 
-const FE_ = (id -> "Feature $id").([1, 1, 2, 3, 2, 3, 4])
+Nucleus.FeatureXSample.write_block(TS_, NAR_, RO___, CO_, MA_)
 
 # ---- #
 
-const SA_ = ["Sample 1"]
+for ts in TS_
+
+    @test isfile(ts)
+
+end
 
 # ---- #
 
-const FEU_ = unique(FE_)
+for (na_, an___) in ((
+    ["Row", "Column 1", "Column 2", "Column 3"],
+    ([1, 2, 3], ['A', 'B', 'C'], ['A', 'B', 'B'], ['C', 'C', 'C']),
+),)
+
+    Nucleus.FeatureXSample.count_unique(na_, an___)
+
+end
 
 # ---- #
 
-const FE2U_ = replace.(FEU_, "Feature" => "New Feature")
+const RO_ = (id -> "Row $id").([1, 1, 2, 3, 2, 3, 4])
+
+# ---- #
+
+const ROU_ = unique(RO_)
+
+# ---- #
+
+const RO2U_ = replace.(ROU_, "Row" => "New Row")
 
 # ---- #
 
 @test Nucleus.FeatureXSample.transform(
-    Nucleus.TE,
-    FE_,
-    SA_,
+    RO_,
+    (id -> "Column $id").(1:3),
     [-1.0; 1; 0; -2; 2; 8; 7;;];
-    fe_fe2 = Dict(zip(FEU_, FE2U_)),
-    fu = Nucleus.FeatureXSample.median,
-    ty = Float64,
+    ro_ro2 = Dict(zip(ROU_, RO2U_)),
     lo = true,
-    naf = "FF",
-    nas = "SS",
-    nan = "NN",
-) == (FE2U_, [0.0; 1; 2; 3;;])
-
-# ---- #
-
-for fi in ("ff_x_ss_x_nn.html", "ffssnn.html", "ffssnn_plus1_log2.html")
-
-    @test isfile(joinpath(Nucleus.TE, fi))
-
-end
+) == (RO2U_, [0.0; 1; 2; 3;;])
