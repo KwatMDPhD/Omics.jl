7e80a52a8dbfefc1cc43dc3c506c72d84a1c26ef
KwatMDPhD <kwat.me@icloud.com>
2024-10-23 21:03:50 -0600

    Split Evidence into Evidence and Fit.

--- src/Evidence.jl
+++ src/Evidence.jl
@@ -14,101 +14,6 @@ function get_posterior_probability(p1, ev)
 
 end
 
-function _range(mi, ma, ex)
-
-    ex *= ma - mi
-
-    mi - ex, ma + ex
-
-end
-
-function plot(ht, ns, sa_, nt, ta_, nf, fe_, p1f_, lo_, up_; si = 4)
-
-    id_ = sortperm(fe_)
-
-    sa_ = sa_[id_]
-
-    ta_ = ta_[id_]
-
-    fe_ = fe_[id_]
-
-    # TODO: Consider sorting p1f_.
-
-    hd = Omics.Color.RE
-
-    hf = Omics.Color.BL
-
-    bo = Dict("yaxis" => "y3", "x" => sa_, "mode" => "lines", "line" => Dict("width" => 0))
-
-    Omics.Plot.plot(
-        ht,
-        (
-            Dict(
-                "type" => "heatmap",
-                "y" => (nt,),
-                "x" => sa_,
-                "z" => (ta_,),
-                "colorscale" => Omics.Palette.fractionate(Omics.Palette.BI),
-                "showscale" => false,
-            ),
-            Dict(
-                "yaxis" => "y2",
-                "y" => fe_,
-                "x" => sa_,
-                "mode" => "markers",
-                "marker" => Dict("size" => si * 2, "color" => hd),
-            ),
-            merge(bo, Dict("y" => lo_)),
-            merge(
-                bo,
-                Dict(
-                    "y" => up_,
-                    "fill" => "tonexty",
-                    "fillcolor" => Omics.Color.hexify(hf, 0.16),
-                ),
-            ),
-            Dict(
-                "yaxis" => "y3",
-                "y" => p1f_,
-                "x" => sa_,
-                "marker" => Dict("size" => si, "color" => hf),
-            ),
-            Dict(
-                "yaxis" => "y3",
-                "y" => (0.5, 0.5),
-                "x" => (sa_[1], sa_[end]),
-                "mode" => "lines",
-                "line" => Dict("color" => Omics.Color.GR),
-            ),
-        ),
-        Dict(
-            "showlegend" => false,
-            "yaxis" => Dict("domain" => (0.98, 1), "ticks" => ""),
-            "yaxis2" => Dict(
-                "domain" => (0, 0.96),
-                "position" => 0,
-                "title" => Dict("text" => nf, "font" => Dict("color" => hd)),
-                "range" => _range(extrema(fe_)..., 0.04),
-                "tickvals" => Omics.Plot.make_tickvals(fe_),
-            ),
-            "yaxis3" => Dict(
-                "overlaying" => "y2",
-                "title" =>
-                    Dict("text" => "Probability of $nt", "font" => Dict("color" => hf)),
-                "range" => _range(0, 1, 0.04),
-                "tickvals" => (0, 0.5, 1),
-            ),
-            "xaxis" => Dict(
-                "anchor" => "y2",
-                "domain" => (0.08, 1),
-                "title" => Dict("text" => ns),
-                "ticks" => "",
-            ),
-        ),
-    )
-
-end
-
 function _root(ev)
 
     sign(ev) * sqrt(abs(ev))
@@ -197,7 +102,7 @@ function plot(ht, nt, p1, nf_, p1f_, ac = nothing; xe = 8)
 
     ye = lastindex(nf_) + 1
 
-    yi, ya = _range(0, ye, 0.064)
+    yi, ya = Omics.Plot.rang(0, ye, 0.064)
 
     wi = 4
 
@@ -244,7 +149,7 @@ function plot(ht, nt, p1, nf_, p1f_, ac = nothing; xe = 8)
             "xaxis" => Dict(
                 "side" => "top",
                 "title" => Dict("text" => "Evidence for $nt", "standoff" => 40),
-                "range" => _range(-xe, xe, 0.02),
+                "range" => Omics.Plot.rang(-xe, xe, 0.02),
                 "tickvals" => ti_,
                 "ticktext" => map(
                     ev ->
--- test/Evidence.jl
+++ test/Evidence.jl
@@ -6,6 +6,20 @@ using Test: @test
 
 # ---- #
 
+# 3.041 ns (0 allocations: 0 bytes)
+# 3.041 ns (0 allocations: 0 bytes)
+# 3.625 ns (0 allocations: 0 bytes)
+# 3.625 ns (0 allocations: 0 bytes)
+# 3.375 ns (0 allocations: 0 bytes)
+# 3.333 ns (0 allocations: 0 bytes)
+# 3.333 ns (0 allocations: 0 bytes)
+# 5.125 ns (0 allocations: 0 bytes)
+# 4.583 ns (0 allocations: 0 bytes)
+# 3.666 ns (0 allocations: 0 bytes)
+# 6.458 ns (0 allocations: 0 bytes)
+# 4.583 ns (0 allocations: 0 bytes)
+# 6.458 ns (0 allocations: 0 bytes)
+# 4.583 ns (0 allocations: 0 bytes)
 for (p1, p1f, ev) in (
     # 0 / 0
     (0, 0, NaN),
@@ -31,11 +45,15 @@ for (p1, p1f, ev) in (
     (1 / 3, 0.5, 1.0000000000000002),
 )
 
-    @test Omics.Evidence.get_evidence(p1, p1f) === ev
+    @test Omics.Evidence.ge(p1, p1f) === ev
+
+    #@btime Omics.Evidence.ge($p1, $p1f)
 
     if isfinite(ev)
 
         @test Omics.Evidence.get_posterior_probability(p1, ev) === p1f
+
+        #@btime Omics.Evidence.get_posterior_probability($p1, $ev)
 
     end
 
@@ -50,7 +68,7 @@ Omics.Plot.plot(
     [
         Dict(
             "name" => "Prior = $p1",
-            "y" => map(p1f -> Omics.Evidence.get_evidence(p1, p1f), P1F_),
+            "y" => map(p1f -> Omics.Evidence.ge(p1, p1f), P1F_),
             "x" => P1F_,
         ) for p1 in P1_
     ],
@@ -62,30 +80,13 @@ Omics.Plot.plot(
 
 # ---- #
 
-for (ta_, fe_, p1f_) in (
-    ([0, 0, 0, 1, 1, 1], [0, 0, 0, 2, 2, 2], [0, 0, 0, 1, 1, 1]),
-    ([1, 1, 1, 0, 0, 0], [2, 2, 2, 0, 0, 0], [1, 1, 1, 0, 0, 0]),
-    ([0, 1, 0, 1, 0, 1], [0, 2, 0, 2, 0, 2], [0, 1, 0, 1, 0, 1]),
-    ([0, 0, 0, 1, 1, 1], [2, 2, 2, 0, 0, 0], [0, 0, 0, 1, 1, 1]),
-    ([1, 1, 1, 0, 0, 0], [0, 0, 0, 2, 2, 2], [1, 1, 1, 0, 0, 0]),
-    ([0, 1, 0, 1, 0, 1], [2, 0, 2, 0, 2, 0], [0, 1, 0, 1, 0, 1]),
+Omics.Evidence.plot(
+    "",
+    "Something",
+    0.6,
+    ("Aa = 0.5", "Bb = 0.6", "Cc = 0.7", "Dd = 0.8", "Ee = 0.9"),
+    (0.5, 0.6, 0.7, 0.8, 0.9),
 )
-
-    Omics.Evidence.plot(
-        "",
-        "Sample",
-        map(id -> "_$id", eachindex(ta_)),
-        "Target",
-        ta_,
-        "Feature",
-        fe_,
-        p1f_,
-        map(nu -> nu - 0.1, p1f_),
-        map(nu -> nu + 0.1, p1f_);
-        si = 24,
-    )
-
-end
 
 # ---- #
 
@@ -94,13 +95,3 @@ for uf in 1:8
     Omics.Evidence.plot("", "Target", 0.4, ["Feature $id = 1.234" for id in 1:uf], rand(uf))
 
 end
-
-# ---- #
-
-Omics.Evidence.plot(
-    "",
-    "Something",
-    0.6,
-    ("Aa = 0.5", "Bb = 0.6", "Cc = 0.7", "Dd = 0.8", "Ee = 0.9"),
-    (0.5, 0.6, 0.7, 0.8, 0.9),
-)
