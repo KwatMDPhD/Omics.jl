627cb874495cd71606a574c97c403022b303b5f9
KwatMDPhD <kwat.me@icloud.com>
2024-12-20 16:35:19 -0700

    HTM.

--- src/HTM.jl
+++ src/HTM.jl
@@ -1,6 +1,6 @@
 module HTM
 
-function writ(ht, sr_, id, sc, ba = "#000000")
+function writ(ht, sr_, id, sc, ba = "#ff0000")
 
     write(
         ht,
@@ -10,12 +10,12 @@ function writ(ht, sr_, id, sc, ba = "#000000")
           <head>
             <meta charset="utf-8" />
           </head>
-          $(join(("<script src=\"$sr\"></script>" for sr in sr_), '\n'))
-          <body style="height: 100vh; width: 100vw; background: $ba">
+        $(join(("<script src=\"$sr\"></script>" for sr in sr_), '\n'))
+          <body style="height: 100vh; width: 100vw; background: $ba; margin: 0">
             <div id="$id" style="height: 100%; width: 100%"></div>
           </body>
           <script>
-            $sc
+        $sc
           </script>
         </html>""",
     )
--- test/HTM.jl
+++ test/HTM.jl
@@ -15,12 +15,9 @@ const HT = Omics.HTM.writ(
     """
     SCRIPT LINE 1
     SCRIPT LINE 2""",
-    "#ff0000",
 )
 
 const ST = read(HT, String)
-
-println(ST)
 
 @test count(==('\n'), ST) === 14
 
@@ -34,7 +31,7 @@ const ID = "id"
 
 Omics.Path.ope(
     Omics.HTM.writ(
-        HT,
+        joinpath(tempdir(), "pl.html"),
         ("https://cdn.plot.ly/plotly-2.35.2.min.js",),
         ID,
         """
@@ -54,7 +51,7 @@ Omics.Path.ope(
 
 Omics.Path.ope(
     Omics.HTM.writ(
-        HT,
+        joinpath(tempdir(), "cy.html"),
         ("https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.29.2/cytoscape.min.js",),
         ID,
         """
@@ -62,7 +59,7 @@ Omics.Path.ope(
             container: document.getElementById("$ID"),
             elements: $(json((
                           Dict("data" => Dict("id" => "A"), "position" => Dict("y" => 0, "x" => 0)),
-                          Dict("data" => Dict("id" => "B"), "position" => Dict("y" => 100, "x" => 100)),
+                          Dict("data" => Dict("id" => "B"), "position" => Dict("y" => 80, "x" => 80)),
                       ))),
             layout: $(json(Dict("name" => "preset"))),
         })""",
