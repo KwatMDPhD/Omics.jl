f4c999f5f858bf0ea49dc8a7449c95e637fe8a8f
KwatMDPhD <kwat.me@icloud.com>
2024-12-22 02:17:28 -0700

    Information. HTM.

--- src/HTM.jl
+++ src/HTM.jl
@@ -11,8 +11,8 @@ function writ(ht, sr_, id, sc, ba = "#000000")
             <meta charset="utf-8" />
           </head>
         $(join(("<script src=\"$sr\"></script>" for sr in sr_), '\n'))
-          <body style="height: 100vh; background: $ba; margin: 0">
-            <div id="$id" style="height: 100%; min-height: fit-content; min-width: fit-content; display: flex; justify-content: center; align-items: center"></div>
+          <body style="background: $ba; margin: 0">
+            <div id="$id" style="min-height: 100vh; min-width: fit-content; display: flex; justify-content: center; align-items: center"></div>
           </body>
           <script>
         $sc
