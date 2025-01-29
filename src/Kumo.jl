33add35835eaf7554aea9dcf89f73f64fa47120e
KwatMDPhD <kwat.me@icloud.com>
2025-01-29 12:15:44 -0700

    Kumoing

--- src/Kumo.jl
+++ src/Kumo.jl
@@ -209,7 +209,7 @@ function plot(
 
     if !isempty(ep_)
 
-        position!(en_, ep_)
+        Omics.Cytoscape.position!(en_, ep_)
 
     end
 
@@ -229,7 +229,7 @@ function plot(
                     "selector" => "#$(en["data"]["id"])",
                     "style" => Dict(
                         "background-color" =>
-                            Omics.Palette.color(he, Omics.Palette.bwr),
+                            Omics.Palette.color(he, Omics.Palette.bwr)[1:7],
                     ),
                 ),
             )
@@ -333,269 +333,238 @@ function plot(
 
 end
 
-# TODO: Test.
-#function find(no_al_, fe_)
-#
-#    for (no, al_) in no_al_
-#
-#        if isempty(al_)
-#
-#            continue
-#
-#        end
-#
-#        @info "🥷 $no = $(join(al_, "  "))"
-#
-#        for al in al_
-#
-#            la = lowercase(al)
-#
-#            for fe in fe_
-#
-#                lf = lowercase(fe)
-#
-#                if la == lf
-#
-#                    @info "🎯 $fe."
-#
-#                elseif contains(lf, la)
-#
-#                    @info "💣 $fe."
-#
-#                end
-#
-#            end
-#
-#        end
-#
-#    end
-#
-#end
-#
 # TODO: Consider `+1`.
-#function heat(fe_sc; no_al_ = Dict{String, Vector{String}}())
-#
-#    he_ = zeros(length(NO_))
-#
-#    for (id, no) in enumerate(NO_)
-#
-#        if haskey(fe_sc, no)
-#
-#            he_[id] = fe_sc[no]
-#
-#        else
-#
-#            sc_ = Float64[]
-#
-#            for al in no_al_[no]
-#
-#                if haskey(fe_sc, al)
-#
-#                    push!(sc_, fe_sc[al])
-#
-#                end
-#
-#            end
-#
-#            if !isempty(sc_)
-#
-#                he_[id] = mean(sc_)
-#
-#            end
-#
-#        end
-#
-#    end
-#
-#    he_
-#
-#end
-#
-#function heat(fe_, fe_x_sa_x_sc; ke_ar...)
-#
-#    no_x_sa_x_he = Matrix{Float64}(undef, length(NO_), size(fe_x_sa_x_sc, 2))
-#
-#    if lastindex(fe_) != size(fe_x_sa_x_sc, 1)
-#
-#        error("Numbers of features differ.")
-#
-#    end
-#
-#    for (id, sc_) in enumerate(eachcol(fe_x_sa_x_sc))
-#
-#        no_x_sa_x_he[:, id] = heat(Dict(zip(fe_, sc_)); ke_ar...)
-#
-#    end
-#
-#    no_x_sa_x_he
-#
-#end
-#
-#function make_edge_matrix()
-#
-#    n = length(NO_)
-#
-#    so_x_ta_x_ed = falses(n, n)
-#
-#    no_id = Dict(no => id for (id, no) in enumerate(NO_))
-#
-#    for (n1, n2) in ED_
-#
-#        so_x_ta_x_ed[no_id[n1], no_id[n2]] = true
-#
-#    end
-#
-#    so_x_ta_x_ed
-#
-#end
-#
-#function play(he_, so_x_ta_x_ed; n_it = 1000, de = 0.5, ch = 0.000001)
-#
-#    n_no = length(NO_)
-#
-#    if !(n_no == lastindex(he_) == size(so_x_ta_x_ed, 1) == size(so_x_ta_x_ed, 2))
-#
-#        error("Numbers of nodes differ.")
-#
-#    end
-#
-#    no_x_id_x_he = Matrix{Float64}(undef, n_no, 1 + n_it)
-#
-#    no_x_id_x_he[:, 1] = he_
-#
-#    en = enumerate(zip(eachcol(so_x_ta_x_ed), NO_))
-#
-#    for id in 1:n_it
-#
-#        id1 = id + 1
-#
-#        ex = exp((1 - id) * de)
-#
-#        for (idt, (ed_, at)) in en
-#
-#            cu = no_x_id_x_he[idt, id]
-#
-#            if !any(ed_)
-#
-#                no_x_id_x_he[idt, id1] = cu
-#
-#                continue
-#
-#            end
-#
-#            he_ = no_x_id_x_he[ed_, id]
-#
-#            me = mean(he_)
-#
-#            if contains(at, '.')
-#
-#                if any(iszero, he_)
-#
-#                    no_x_id_x_he[idt, id1] = 0.0
-#
-#                    continue
-#
-#                end
-#
-#                n_ch = lastindex(at)
-#
-#                no_x_id_x_he[idt, id1] = view(at, (n_ch - 1):n_ch) == "de" ? -me : me
-#
-#            else
-#
-#                no_x_id_x_he[idt, id1] = max(0.0, cu + me * ex)
-#
-#            end
-#
-#        end
-#
-#        if abs(norm(view(no_x_id_x_he, :, id1)) - norm(view(no_x_id_x_he, :, id))) < ch
-#
-#            return no_x_id_x_he[:, 1:id1]
-#
-#        elseif id == n_it
-#
-#            error("Failed to converge.")
-#
-#        end
-#
-#    end
-#
-#end
-#
+function heat(fe_sc; no_al_ = Dict{String, Vector{String}}())
+
+    he_ = zeros(length(NO_))
+
+    for id in eachindex(NO_)
+
+        no = NO_[id]
+
+        if contains(no, '.')
+
+            continue
+
+        end
+
+        if haskey(fe_sc, no)
+
+            he_[id] = fe_sc[no]
+
+        else
+
+            sc_ = Float64[]
+
+            for al in no_al_[no]
+
+                if haskey(fe_sc, al)
+
+                    push!(sc_, fe_sc[al])
+
+                end
+
+            end
+
+            if !isempty(sc_)
+
+                he_[id] = mean(sc_)
+
+            end
+
+        end
+
+    end
+
+    he_
+
+end
+
+function heat(fe_, fe_x_sa_x_sc; ke_ar...)
+
+    no_x_sa_x_he = Matrix{Float64}(undef, length(NO_), size(fe_x_sa_x_sc, 2))
+
+    if lastindex(fe_) != size(fe_x_sa_x_sc, 1)
+
+        error("Numbers of features differ.")
+
+    end
+
+    for (id, sc_) in enumerate(eachcol(fe_x_sa_x_sc))
+
+        no_x_sa_x_he[:, id] = heat(Dict(zip(fe_, sc_)); ke_ar...)
+
+    end
+
+    no_x_sa_x_he
+
+end
+
+function make_edge_matrix()
+
+    n = length(NO_)
+
+    so_x_ta_x_ed = falses(n, n)
+
+    no_id = Dict(no => id for (id, no) in enumerate(NO_))
+
+    for (n1, n2) in ED_
+
+        so_x_ta_x_ed[no_id[n1], no_id[n2]] = true
+
+    end
+
+    so_x_ta_x_ed
+
+end
+
+function play(he_, so_x_ta_x_ed; n_it = 1000, de = 0.5, ch = 0.000001)
+
+    n_no = length(NO_)
+
+    if !(n_no == lastindex(he_) == size(so_x_ta_x_ed, 1) == size(so_x_ta_x_ed, 2))
+
+        error("Numbers of nodes differ.")
+
+    end
+
+    no_x_id_x_he = Matrix{Float64}(undef, n_no, 1 + n_it)
+
+    no_x_id_x_he[:, 1] = he_
+
+    en = enumerate(zip(eachcol(so_x_ta_x_ed), NO_))
+
+    for id in 1:n_it
+
+        id1 = id + 1
+
+        ex = exp((1 - id) * de)
+
+        for (idt, (ed_, at)) in en
+
+            cu = no_x_id_x_he[idt, id]
+
+            if !any(ed_)
+
+                no_x_id_x_he[idt, id1] = cu
+
+                continue
+
+            end
+
+            he_ = no_x_id_x_he[ed_, id]
+
+            me = mean(he_)
+
+            if contains(at, '.')
+
+                if any(iszero, he_)
+
+                    no_x_id_x_he[idt, id1] = 0.0
+
+                    continue
+
+                end
+
+                n_ch = lastindex(at)
+
+                no_x_id_x_he[idt, id1] = view(at, (n_ch - 1):n_ch) == "de" ? -me : me
+
+            else
+
+                no_x_id_x_he[idt, id1] = max(0.0, cu + me * ex)
+
+            end
+
+        end
+
+        if abs(norm(view(no_x_id_x_he, :, id1)) - norm(view(no_x_id_x_he, :, id))) < ch
+
+            return no_x_id_x_he[:, 1:id1]
+
+        elseif id == n_it
+
+            error("Failed to converge.")
+
+        end
+
+    end
+
+end
+
 # TODO: Test.
-#function play!(no_x_sa_x_he, so_x_ta_x_ed; ke_ar...)
-#
-#    for (id, he_) in enumerate(eachcol(no_x_sa_x_he))
-#
-#        no_x_sa_x_he[:, id] = play(he_, so_x_ta_x_ed; ke_ar...)[:, end]
-#
-#    end
-#
-#end
-#
-#function animate(di, el_, no_x_id_x_he; pe = 0, st_ = Dict{String, Any}[])
-#
-#    n = size(no_x_id_x_he, 2)
-#
-#    @info "Animating $(Omics.Strin.count(n, "set")) of heat"
-#
-#    if iszero(pe)
-#
-#        ch = true
-#
-#        pe = 1
-#
-#        st = "change"
-#
-#    else
-#
-#        ch = false
-#
-#        if pe < 1
-#
-#            pe = round(Int, n * pe)
-#
-#        end
-#
-#        st = "every $pe"
-#
-#    end
-#
-#    @info "Plotting $st"
-#
-#    pn_ = String[]
-#
-#    pr = joinpath(di, randstring())
-#
-#    for id in 1:n
-#
-#        tw = !isone(id)
-#
-#        if tw && !iszero(id % pe) && id != n
-#
-#            continue
-#
-#        end
-#
-#        pri = "$(pr)_$id"
-#
-#        he_ = no_x_id_x_he[:, id]
-#
-#        if ch && tw
-#
-#            he_ .-= view(no_x_id_x_he, :, id - 1)
-#
-#        end
-#
-#        plot("$pri.html"; el_, he_, st_, ex = "png")
-#
-#        push!(pn_, "$pri.png")
-#
-#    end
-#
-#    Omics.Plot.animate("$pr.gif", pn_)
-#
-#end
+function play!(no_x_sa_x_he, so_x_ta_x_ed; ke_ar...)
+
+    for (id, he_) in enumerate(eachcol(no_x_sa_x_he))
+
+        no_x_sa_x_he[:, id] = play(he_, so_x_ta_x_ed; ke_ar...)[:, end]
+
+    end
+
+end
+
+function animate(di, el_, no_x_id_x_he; pe = 0, st_ = Dict{String, Any}[])
+
+    n = size(no_x_id_x_he, 2)
+
+    @info "Animating $(Omics.Strin.count(n, "set")) of heat"
+
+    if iszero(pe)
+
+        ch = true
+
+        pe = 1
+
+        st = "change"
+
+    else
+
+        ch = false
+
+        if pe < 1
+
+            pe = round(Int, n * pe)
+
+        end
+
+        st = "every $pe"
+
+    end
+
+    @info "Plotting $st"
+
+    pn_ = String[]
+
+    pr = joinpath(di, randstring())
+
+    for id in 1:n
+
+        tw = !isone(id)
+
+        if tw && !iszero(id % pe) && id != n
+
+            continue
+
+        end
+
+        pri = "$(pr)_$id"
+
+        he_ = no_x_id_x_he[:, id]
+
+        if ch && tw
+
+            he_ .-= view(no_x_id_x_he, :, id - 1)
+
+        end
+
+        plot("$pri.html"; el_, he_, st_, ex = "png")
+
+        push!(pn_, "$pri.png")
+
+    end
+
+    Omics.Plot.animate("$pr.gif", pn_)
+
+end
 
 end
--- test/Kumo.jl
+++ test/Kumo.jl
@@ -14,11 +14,7 @@ function test_collection(no_, cl___, ed_, pl = true)
 
     @test Tuple(Omics.Kumo.ED_) == ed_
 
-    if pl
-
-        Omics.Kumo.plot("")
-
-    end
+    Omics.Kumo.plot("")
 
 end
 
@@ -281,6 +277,10 @@ const GP_ = ("Ligand", "GPCR", "GPCRLigand", "GP", "GPCRGP", "GOn", "GPCRDone", 
 
 const UG = lastindex(GP_)
 
+const PR = joinpath(tempdir(), "GPCR")
+
+# ---- #
+
 clear_st(GP_)
 
 (Ligand, GPCR) >> GPCRLigand
@@ -310,164 +310,131 @@ test_collection(
     false,
 )
 
-Omics.Kumo.plot(""; ex = "json")
+Omics.Kumo.plot("$PR.html"; ex = "json")
 
-## ---- #
-#
-#const EL_ = Nucleus.Graph.read(joinpath(TE, "$PR.json"))
-#
-## ---- #
-#
-#const SC_ = vcat(range(0, 1, UG), Inf)
-#
-## ---- #
-#
-#const FE_SC = Dict(zip(GP_, SC_))
-#
-## ---- #
-#
-#FE_SC["IMU"] = SC_[end]
-#
-## ---- #
-#
-#const HE_ = Omics.Kumo.heat(FE_SC)
-#
-## ---- #
-#
-#const RE = vcat(SC_[1:(end - 1)], zeros(4))
-#
-## ---- #
-#
-#@test HE_ == RE
-#
-## ---- #
-#
-## 429.226 ns (7 allocations: 752 bytes)
-##@btime Omics.Kumo.heat(FE_SC);
-#
-## ---- #
-#
-#Omics.Kumo.plot(""; el_ = EL_, he_ = HE_)
-#
-## ---- #
-#
-#const FE_X_SA_X_SC = hcat(SC_, 10SC_)
-#
-## ---- #
-#
-#@test Nucleus.Error.@is Omics.Kumo.heat(GP_, FE_X_SA_X_SC)
-#
-## ---- #
-#
-#const FE_ = vcat(collect(GP_), "IMU")
-#
-## ---- #
-#
-#const NO_X_SA_X_HE = Omics.Kumo.heat(FE_, FE_X_SA_X_SC)
-#
-## ---- #
-#
-#@test NO_X_SA_X_HE == hcat(RE, 10RE)
-#
-## ---- #
-#
-## 1.242 μs (19 allocations: 2.25 KiB)
-##@btime Omics.Kumo.heat(FE_, FE_X_SA_X_SC);
-#
-## ---- #
-#
-#const SO_X_TA_X_ED = Omics.Kumo.make_edge_matrix()
-#
-## ---- #
-#
-#@test sum(SO_X_TA_X_ED) === length(Omics.Kumo.ED_)
-#
-## ---- #
-#
-## 709.835 ns (10 allocations: 2.08 KiB)
-##@btime Omics.Kumo.make_edge_matrix();
-#
-## ---- #
-#
-#const NO_X_ID_X_HE = Omics.Kumo.play(HE_, SO_X_TA_X_ED)
-#
-## ---- #
-#
-#@test size(NO_X_ID_X_HE, 2) === 30
-#
-## ---- #
-#
-#@test sum.(eachcol(NO_X_ID_X_HE)) == [
-#    4.0,
-#    3.928571428571428,
-#    4.361807614080452,
-#    4.841196736243137,
-#    5.204461981358573,
-#    5.451465133897608,
-#    5.611092068362889,
-#    5.711520312695818,
-#    5.7737610249366345,
-#    5.812000433388874,
-#    5.835373519146523,
-#    5.849616124665676,
-#    5.858279022978179,
-#    5.863542283746393,
-#    5.866737904314711,
-#    5.868677357056958,
-#    5.869854140070007,
-#    5.870568058923495,
-#    5.871001132883382,
-#    5.871263827696215,
-#    5.871423168313263,
-#    5.871519816284339,
-#    5.871578437346185,
-#    5.871613993223707,
-#    5.871635559102994,
-#    5.871648639524959,
-#    5.871656573222147,
-#    5.871661385260175,
-#    5.871664303911514,
-#    5.871666074164041,
-#]
-## ---- #
-#
-#const REA = [
-#    0.0,
-#    0.14285714285714285,
-#    0.2857142857142857,
-#    0.42857142857142855,
-#    1.1219617145500707,
-#    1.7894690854152187,
-#    0.62398814233751,
-#    1.0,
-#    0.0,
-#    0.3571428571428571,
-#    1.1219614175755281,
-#    -1.0,
-#]
-#
-## ---- #
-#
-#@test NO_X_ID_X_HE[:, end] == REA
-#
-## ---- #
-#
-## 15.625 μs (264 allocations: 113.62 KiB)
-##@btime Omics.Kumo.play(HE_, SO_X_TA_X_ED);
-#
-## ---- #
-#
-#@test isapprox(Omics.Kumo.play(NO_X_SA_X_HE, SO_X_TA_X_ED), hcat(REA, 10REA); atol = 0.0001)
-#
-## ---- #
-#
-## 34.333 μs (574 allocations: 230.89 KiB)
-##@btime Omics.Kumo.play(NO_X_SA_X_HE, SO_X_TA_X_ED);
-#
-## ---- #
-#
-#for pe in (0, 0.5, 1)
-#
-#    Omics.Kumo.animate(TE, EL_, NO_X_ID_X_HE; pe)
-#
-#end
+const EP_ = Omics.Cytoscape.rea("$PR.json")
+
+# ---- #
+
+const SC_ = vcat(range(0, 1, UG), Inf)
+
+const FE_SC = Dict(zip(GP_, SC_))
+
+FE_SC["Kaido"] = SC_[end]
+
+const HE_ = Omics.Kumo.heat(FE_SC)
+
+const R1 = vcat(SC_[1:(end - 1)], zeros(4))
+
+@test HE_ == R1
+
+# 429.226 ns (7 allocations: 752 bytes)
+#@btime Omics.Kumo.heat(FE_SC);
+
+Omics.Kumo.plot(""; ep_ = EP_, he_ = HE_)
+
+# ---- #
+
+const SC = hcat(SC_, SC_ * 10)
+
+@test try
+
+    Omics.Kumo.heat(GP_, SC)
+
+catch
+
+    true
+
+end
+
+const FE_ = vcat(collect(GP_), "Kaido")
+
+const HE = Omics.Kumo.heat(FE_, SC)
+
+@test HE == hcat(R1, R1 * 10)
+
+# 1.242 μs (19 allocations: 2.25 KiB)
+#@btime Omics.Kumo.heat(FE_, SC);
+
+# ---- #
+
+const ED = Omics.Kumo.make_edge_matrix()
+
+@test sum(ED) === length(Omics.Kumo.ED_)
+
+# 709.835 ns (10 allocations: 2.08 KiB)
+#@btime Omics.Kumo.make_edge_matrix();
+
+# ---- #
+
+const HI = Omics.Kumo.play(HE_, ED)
+
+@test size(HI, 2) === 30
+
+@test map(sum, eachcol(HI)) == [
+    4.0,
+    3.928571428571428,
+    4.361807614080452,
+    4.841196736243137,
+    5.204461981358573,
+    5.451465133897608,
+    5.611092068362889,
+    5.711520312695818,
+    5.7737610249366345,
+    5.812000433388874,
+    5.835373519146523,
+    5.849616124665676,
+    5.858279022978179,
+    5.863542283746393,
+    5.866737904314711,
+    5.868677357056958,
+    5.869854140070007,
+    5.870568058923495,
+    5.871001132883382,
+    5.871263827696215,
+    5.871423168313263,
+    5.871519816284339,
+    5.871578437346185,
+    5.871613993223707,
+    5.871635559102994,
+    5.871648639524959,
+    5.871656573222147,
+    5.871661385260175,
+    5.871664303911514,
+    5.871666074164041,
+]
+
+const R2 = [
+    0.0,
+    0.14285714285714285,
+    0.2857142857142857,
+    0.42857142857142855,
+    1.1219617145500707,
+    1.7894690854152187,
+    0.62398814233751,
+    1.0,
+    0.0,
+    0.3571428571428571,
+    1.1219614175755281,
+    -1.0,
+]
+
+@test HI[:, end] == R2
+
+# 15.625 μs (264 allocations: 113.62 KiB)
+#@btime Omics.Kumo.play(HE_, ED);
+
+# ---- #
+
+@test isapprox(Omics.Kumo.play(HE, ED), hcat(R2, R2 * 10); atol = 0.0001)
+
+# 34.333 μs (574 allocations: 230.89 KiB)
+#@btime Omics.Kumo.play(HE, ED);
+
+# ---- #
+
+for pe in (0, 0.5, 1)
+
+    Omics.Kumo.animate(TE, EP_, HI; pe)
+
+end
