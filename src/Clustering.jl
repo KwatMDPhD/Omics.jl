612a1a04e7e293a63bacc79e9eb881c091724d1f
KwatMDPhD <kwat.me@icloud.com>
2025-02-11 14:33:05 -0700

    Clustering.

--- src/Clustering.jl
+++ src/Clustering.jl
@@ -4,24 +4,24 @@ using Clustering: hclust
 
 using Distances: pairwise
 
-function hierarchize(di, linkage = :ward)
+function hierarchize(di)
 
-    hclust(di; linkage)
+    hclust(di; linkage = :ward, branchorder = :barjoseph)
 
 end
 
-function order(fu, gr_, an___)
+function order(di, gr_, an___)
 
     i1_ = Vector{Int}(undef, lastindex(gr_))
 
-    id = 0
+    i2 = 0
 
     for un in sort!(unique(gr_))
 
-        i2_ = findall(==(un), gr_)
+        i3_ = findall(==(un), gr_)
 
-        i1_[(id + 1):(id += lastindex(i2_))] =
-            i2_[hierarchize(pairwise(fu, an___[i2_])).order]
+        i1_[(i2 + 1):(i2 += lastindex(i3_))] =
+            i3_[hierarchize(pairwise(di, an___[i3_])).order]
 
     end
 
--- test/Clustering.jl
+++ test/Clustering.jl
@@ -6,29 +6,31 @@ using Omics
 
 # ---- #
 
-const NU = [
-    0 1 2 3 10 20 30
-    0 2 1 3 20 10 30
-    0 1 2 3 10 20 30
-    0.1 2 1 3.1 20 10 30.1
+const AN = [
+    0 1 2 3 10 20 30 40
+    0 2 1 3 20 10 30 40
+    0 1 2 3 10 20 30 40
+    0.1 2 1 3.1 20 10 30.1 40.1
+    0.2 2 1 3.2 20 10 30.2 40.2
+    0.3 2 1 3.3 20 10 30.3 40.3
 ]
 
-for (ea, re) in ((eachcol, [4, 1, 2, 3, 7, 5, 6]), (eachrow, [1, 3, 2, 4]))
+for (ea, re) in ((eachcol, [1, 3, 2, 4, 6, 5, 7, 8]), (eachrow, [1, 3, 2, 4, 5, 6]))
 
-    @test Omics.Clustering.hierarchize(pairwise(Euclidean(), ea(NU))).order == re
+    @test Omics.Clustering.hierarchize(pairwise(Euclidean(), ea(AN))).order == re
 
 end
 
 # ---- #
 
-# 1.696 μs (128 allocations: 7.19 KiB)
-# 1.721 μs (128 allocations: 7.19 KiB)
-# 1.725 μs (128 allocations: 7.19 KiB)
-# 1.721 μs (128 allocations: 7.19 KiB)
-# 1.683 μs (128 allocations: 7.19 KiB)
-# 1.717 μs (128 allocations: 7.19 KiB)
+# 2.093 μs (146 allocations: 8.28 KiB)
+# 2.116 μs (146 allocations: 8.28 KiB)
+# 2.069 μs (146 allocations: 8.28 KiB)
+# 2.088 μs (146 allocations: 8.28 KiB)
+# 2.079 μs (146 allocations: 8.28 KiB)
+# 2.106 μs (146 allocations: 8.28 KiB)
 
-for (gr_, nu, re) in (
+for (gr_, an, re) in (
     (
         [1, 1, 1, 1, 2, 2, 2, 2],
         [
@@ -79,10 +81,10 @@ for (gr_, nu, re) in (
     ),
 )
 
-    nu___ = eachcol(nu)
+    an___ = eachcol(an)
 
-    @test Omics.Clustering.order(Euclidean(), gr_, nu___) == re
+    @test Omics.Clustering.order(Euclidean(), gr_, an___) == re
 
-    #@btime Omics.Clustering.order(Euclidean(), $gr_, $nu___)
+    #@btime Omics.Clustering.order(Euclidean(), $gr_, $an___)
 
 end
